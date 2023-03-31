6ed64df919e7e0f1c5ea0b552b3bf62b6af1c219
Michal Motowidlo <motowidlo94@gmail.com>
2023-03-31 08:12:02 +0200

    adding new tasks

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 {
-    const tasks = [ 
+    const tasks = [
         {
             content: "zrobić zadanie",
             done: false,
@@ -9,22 +9,49 @@
             done: true,
         },
     ];
-    
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
-            htmlString +=`
-            <li>
+            htmlString += `
+            <li
+             ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+            >
             ${task.content}
             </li>`
         }
         document.querySelector(".js-list").innerHTML = htmlString;
     };
-    
-    
-    const init = () => {
-    render();
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
     }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    }
+
+
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
     init();
 }
